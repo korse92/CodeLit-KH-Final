dc41f44fefb73959d7520563d3901e4544cd6aec
jungdami-ing <jungdamiing@gmail.com>
2021-05-14 01:59:49 +0900

    아이디 중복확인 (chkIdDupl)

--- src/main/java/com/kh/codelit/member/controller/MemberController.java
+++ src/main/java/com/kh/codelit/member/controller/MemberController.java
@@ -1,13 +1,21 @@
 package com.kh.codelit.member.controller;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.codelit.member.model.service.MemberService;
@@ -28,11 +36,9 @@ public class MemberController {
 	
 	@GetMapping("/memberEnroll.do")
 	public void memberEnroll() {
-		log.debug("회원가입 = {}", "회원가입");
 	}
 	
-//	@PostMapping("/memberEnroll.do")
-	@RequestMapping(value = "/memberEnroll.do", method = RequestMethod.POST)
+	@PostMapping("/memberEnroll.do")
 	public String memberEnroll(
 						@ModelAttribute Member member, 
 						RedirectAttributes redirectAttr
@@ -49,7 +55,7 @@ public class MemberController {
 			
 			//1. 업무로직
 			int result = memberService.insertMember(member);
-			String msg =  result > 0 ? "회원 등록 성공!" : "회원 등록 실패!";		
+			String msg =  result > 0 ? "회원가입 되었습니다." : "회원가입에 실패하였습니다.";		
 			//2. 사용자 피드백 준비 및 리다이렉트
 			redirectAttr.addFlashAttribute("msg", msg);
 		} catch(Exception e) {
@@ -57,6 +63,24 @@ public class MemberController {
 			throw e;
 		}
 		return "redirect:/";
+	}
+	
+	//아이디 중복 확인
+	@GetMapping("/chkIdDupl.do")
+	public ResponseEntity<?> chkIdDupl(@RequestParam("id") String memberId) {
+		//1.업무로직
+		Member member = memberService.selectOneMember(memberId);
+		boolean usable = (member == null);
+		
+		//2. json 변환 객체
+		Map<String, Object> map = new HashMap<>();
+		map.put("usable", usable);
+		map.put("id", memberId);
+		map.put("serverTime", new Date());
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		return new ResponseEntity<>(map, headers, HttpStatus.OK);
 	}
 	
 	@GetMapping("/memberLogin.do")
--- src/main/webapp/WEB-INF/views/member/memberEnroll.jsp
+++ src/main/webapp/WEB-INF/views/member/memberEnroll.jsp
@@ -71,8 +71,11 @@
         <!-- 이메일 -->
         <div class="row mb-3">
           <label for="id" class="col-sm-3 col-form-label">이메일(아이디)</label>
-          <div class="col-sm-9">
-            <input type="text" class="form-control" id="id" name="memberId" required>
+          <div class="col-sm-9" id="memberId">
+            <input type="text" class="form-control" id="id" name="id" required>
+            <span class="guide ok">이 아이디는 사용 가능합니다.</span>
+            <span class="guide no">이 아이디는 이미 사용중입니다.</span>
+            <input type="hidden" id=idValid" value="0"/>
           </div>
         </div>
         <div class="row mb-3">
@@ -85,7 +88,7 @@
         <div class="row mb-3">
           <label for="password" class="col-sm-3 col-form-label">비밀번호</label>
           <div class="col-sm-9">
-            <input type="password" class="form-control" id="password" name="memberPw" required>
+            <input type="password" class="form-control" id="password" name="password" required>
           </div>
         </div>
         <div class="row mb-3">
@@ -95,7 +98,7 @@
           </div>
         </div>
         <button type="submit" class="btn btn-primary col-12 mt-3 mb-5" id="submit">가입하기</button>
-        <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}">
+        <%-- <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"> --%>
       </form>
 
     </div>
@@ -113,6 +116,42 @@
   </section>
 
 <script>
+/* 아이디 중복 확인 */
+$("#id").keyup(e => {
+	const id = $(e.target).val();
+	const $no = $(".guide.no");
+	const $ok = $(".guide.ok");
+	const $idValid = $("#idValid");
+	
+	//아이디 처음 작성하거나, 다시 작성하는 경우
+	if(id.length < 4){
+		$(".guide").hide();
+		$idValid.val(0);
+		return;
+	}
+	
+	$.ajax({
+		url: "${pageContext.request.contextPath}/member/chkIdDupl.do",
+		data: {id},
+		success: data => {
+			console.log(data);
+			if(data.usable){
+				$no.hide();
+				$ok.show();
+				$idValid.val(1);
+			}
+			else{
+				$no.show();
+				$ok.hide();
+				$idValid.val(0);
+			}
+		},
+		error: (xhr, status, err) => {
+			console.log(xhr, status, err);
+		}
+	});
+});
+
 /* 아이디 확인 */
 $("#chkId").blur(function(){
 	var $id = $("#id"), $chkId = $("#chkId");
