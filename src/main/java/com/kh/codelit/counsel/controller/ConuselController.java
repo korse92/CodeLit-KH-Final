51fdd94181ea470c483d0896d9d758fda16d74da
hhj0221 <hhj0221@naver.com>
2021-05-22 12:06:16 +0900

    오류사항

--- src/main/java/com/kh/codelit/counsel/controller/ConuselController.java
+++ src/main/java/com/kh/codelit/counsel/controller/ConuselController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -130,7 +129,6 @@ public class ConuselController {
 		}
 		
 	}
-	
 	
 	
 
