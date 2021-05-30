667e12af252287847c7377218866c439f3dbcd70
asrisk94 <asrisk94@gmail.com>
2021-05-30 22:17:23 +0900

    재생 기능 오류 수정

--- src/main/java/com/kh/codelit/lecture/controller/LectureController.java
+++ src/main/java/com/kh/codelit/lecture/controller/LectureController.java
@@ -316,7 +316,7 @@ public class LectureController {
 	@GetMapping("/lecture.do")
 	public void lecture(
 				// 강의번호와 영상챕터번호 받아옴. (영상챕터번호 없다면, 막 상세보기면 -1 배정)
-				@RequestParam(defaultValue = "3") int lectureNo,
+				@RequestParam(defaultValue = "71") int lectureNo,
 				@RequestParam(defaultValue="-1") int chapterNo,
 				Model model,
 				Authentication authentication
@@ -334,7 +334,7 @@ public class LectureController {
 			// 매개변수
 			Map<String, Object> param = new HashMap<>();
 			param.put("refMemberId", refMemberId);
-			param.put("refLectureNo", 3);
+			param.put("refLectureNo", lectureNo);
 
 			// 챕터번호와 영상시청여부 추출 (chapterNo, yn)
 			List<Map<String, Object>> progList = lectureService.selectLectureProgress(param);
